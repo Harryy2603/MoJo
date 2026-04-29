b3aeb261fcf826ec9496795ad675c6b5b6e12d5d
Harryy2603 <153521415+Harryy2603@users.noreply.github.com>
2026-04-29 12:32:23 +0000

    updated url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext  = createContext(null)
 
 const StoreContextProvider = (props) =>{
 
-    const url = "http://localhost:4000"
+    const url = "https://mojo-backend-services.onrender.com"
     const [token,setToken] = useState("");
     const [name, setName] = useState("");
 
